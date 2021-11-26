62faf8ad1ef11a81c03e736c2006c73ac7a107df
André Leão <94919094+andreleao81@users.noreply.github.com>
2021-11-26 11:15:28 -0300

    Removed comments

--- src/pkg/listener1.py
+++ src/pkg/listener1.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 import rospy
-# from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import Twist
-# from geometry_msgs.msg import Point
 from std_msgs.msg import Float32
 
 import math
 
 
 class Listener:
+    
     def __init__(self):
         rospy.init_node('listener1', anonymous=True)
         rospy.Subscriber("vel_components", Twist, self.callback)
@@ -19,13 +18,6 @@ class Listener:
         vel = msg
         vel_linear = math.sqrt(vel.linear.x**2 + vel.linear.y**2 + vel.linear.z**2)
         vel_angular = math.sqrt(vel.angular.x**2 + vel.angular.y**2 + vel.angular.z**2)
-        '''
-        aa = Vector3()
-        ll = Vector3()
-        aa = vel.linear
-        ll = vel.linear
-        
-        '''
         a = Float32()
         l = Float32()
         a.data = vel_angular
